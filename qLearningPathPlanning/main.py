ed1583736133ff6a1662681bae08faf368ae3651
Tayfun Kuşçu <69037566+tayfunkscu@users.noreply.github.com>
2021-04-28 14:33:38 +0300

    update

--- qLearningPathPlanning/main.py
+++ qLearningPathPlanning/main.py
@@ -4,15 +4,31 @@ from pygame.locals import *
 
 from maze import Maze
 
-if __name__ == '__main__':
-    maze = Maze()
-    maze.design()
-    screen = pygame.display.set_mode((maze.resX, maze.resY))
-    while True:
-        screen.fill((191, 191, 191))
-        maze.paint(screen)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
+maze = Maze()
+screen = pygame.display.set_mode((maze.resX, maze.resY))
+selection = True
+while True:
+    screen.fill((191, 191, 191))
+    maze.paint(screen)
+    mouse_pos = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            maze.map2txt()
+            pygame.quit()
+            sys.exit()
+        if selection:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left click
+                cor_y, cor_x = event.pos
+                maze.map[int(cor_x / 15), int(cor_y / 15)] = 1
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:  # Right click
+                cor_y, cor_x = event.pos
+                maze.map[int(cor_x / 15), int(cor_y / 15)] = 2
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 2:
+                selection = False
+        else:
+            cor_y, cor_x = mouse_pos
+            maze.map[int(cor_x / 15), int(cor_y / 15)] = -1 if maze.map[int(cor_x / 15), int(cor_y / 15)] == 0 else \
+                maze.map[int(cor_x / 15), int(cor_y / 15)]
+        # if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left click
+
+    pygame.display.update()
--- qLearningPathPlanning/maze.py
+++ qLearningPathPlanning/maze.py
@@ -6,52 +6,30 @@ class Maze:
     def __init__(self):
         self.x = 50
         self.y = 50
-        self.resX = 1000
-        self.resY = 1000
+        self.cell_size = 15
+        self.resX = 750
+        self.resY = 750
         self.colors = [(52, 52, 52), (191, 191, 191), (43, 191, 27), (0, 123, 255)]  # black, gray, green, blue
         self.map = np.zeros((50, 50))
         self.target = None
         self.agent = None
         self.obstacles = None
 
-    def design(self):
-        for i in range(50):
-            print("{} -> {}".format(i * 50, (i * 50) + 50))
-
-        self.obstacles = list(map(str, input("\nSpecify the cells you would like to put an obstacle: ").split()))
-        self.agent = input("Specify the starting cell for agent: ")
-        self.target = input("Specify the target cell: ")
-
-        for i in self.obstacles:
-            row = int(int(i) / 50)
-            column = int(i) % 50
-            self.map[row, column] = -1
-
-        row = int(int(self.agent) / 50)
-        column = int(self.agent) % 50
-        self.map[row, column] = 1
-
-        row = int(int(self.target) / 50)
-        column = int(self.target) % 50
-        self.map[row, column] = 2
-
-        self.map2txt()
-
     def paint(self, screen):
         for i in range(50):
             for j in range(50):
-                pygame.draw.rect(screen, (255, 255, 255), (j * 20, i * 20, (j * 20) + 20, (i * 20) + 20), 0)
+                pygame.draw.rect(screen, (255, 255, 255), (j * 15, i * 15, (j * 15) + 15, (i * 15) + 15), 0)
                 if self.map[i, j] == -1:
-                    pygame.draw.rect(screen, self.colors[0], ((j * 20) + 3, (i * 20) + 3, (j * 20) + 17, (i * 20) + 17),
+                    pygame.draw.rect(screen, self.colors[0], ((j * 15) + 3, (i * 15) + 3, (j * 15) + 11, (i * 15) + 11),
                                      0)
                 elif self.map[i, j] == 0:
-                    pygame.draw.rect(screen, self.colors[1], ((j * 20) + 3, (i * 20) + 3, (j * 20) + 17, (i * 20) + 17),
+                    pygame.draw.rect(screen, self.colors[1], ((j * 15) + 3, (i * 15) + 3, (j * 15) + 11, (i * 15) + 11),
                                      0)
                 elif self.map[i, j] == 1:
-                    pygame.draw.rect(screen, self.colors[3], ((j * 20) + 3, (i * 20) + 3, (j * 20) + 17, (i * 20) + 17),
+                    pygame.draw.rect(screen, self.colors[3], ((j * 15) + 3, (i * 15) + 3, (j * 15) + 11, (i * 15) + 11),
                                      0)
                 elif self.map[i, j] == 2:
-                    pygame.draw.rect(screen, self.colors[2], ((j * 20) + 3, (i * 20) + 3, (j * 20) + 17, (i * 20) + 17),
+                    pygame.draw.rect(screen, self.colors[2], ((j * 15) + 3, (i * 15) + 3, (j * 15) + 11, (i * 15) + 11),
                                      0)
 
     def map2txt(self):
